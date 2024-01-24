b4aa978e1dcbee9f6c3b969d1fc83ab442ed9891
Farnik Nikakhtar <farnik@sas.upenn.edu>
2024-01-23 20:43:28 -0500

    ads api

--- bin/get_ads.py
+++ bin/get_ads.py
@@ -1,21 +1,31 @@
-import ads
+import ads, re
 
 # Function to determine the kind value based on the entry's type and title content
 def determine_kind(bibtex_entry):
-    if bibtex_entry.strip().startswith('@ARTICLE'):
-        return "Peer Reviewed Journals"
-    elif bibtex_entry.strip().startswith('@INPROCEEDINGS'):
-        return "Conference Proceedings"
-    elif 'data release' in bibtex_entry.lower() or 'database' in bibtex_entry.lower():
-        return "Data Release Papers"
-    else:
-        return None
+    isARTICLE = bibtex_entry.strip().startswith('@ARTICLE')
+    isINPROCEEDINGS = bibtex_entry.strip().startswith('@INPROCEEDINGS')
+
+    title_regex = r'title\s*=\s*"\{([^}]+)\}"'
+    title = re.search(title_regex, bibtex_entry, re.IGNORECASE).group(1)
+
+    hasDR = 'data release' in title.lower()
+    hasDB = 'database' in title.lower()
+    hasTheli = 'theli pipeline' in title.lower()
+
+    if isARTICLE and not hasDR and not hasDB and not hasTheli:
+        kind = "Peer Reviewed Journals"
+    elif isINPROCEEDINGS or hasTheli:
+        kind = "Conference Proceedings"
+    elif hasDR or hasDB:
+        kind = "Data Release Papers"
+
+    return kind
 
 
 def add_kind_to_bibtex(bibtex_entry, kind_value):
     # Determine where to insert the new keyword
     last_comma_index = bibtex_entry.rfind(",")
-    kind_keyword_line = f"       kind = {{{kind_value}}},\n"
+    kind_keyword_line = f"       kind = {{{kind_value}}},"
     # Insert the kind keyword
     updated_entry = bibtex_entry[:last_comma_index+1] + "\n" + kind_keyword_line + bibtex_entry[last_comma_index+1:]
     return updated_entry
@@ -54,7 +64,7 @@ def make_bib(author, outfile="cv.bib"):
             bibquery = ads.ExportQuery(p.bibcode)
             bibs = bibquery.execute()
 
-            new_keyword = "       bibtex_show = {true},\n"
+            new_keyword = "       bibtex_show = {true},"
             last_comma_index = bibs.rfind(",")
             bibs = bibs[:last_comma_index+1] + "\n" + new_keyword + bibs[last_comma_index+1:]
 
